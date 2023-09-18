63a5f26115a50ac441b669ae537b66736e5d763a
tienho <hongoctien@gmail.com>
2023-09-18 17:05:58 +0700

    SS3_TH

---
+++ src/SS3_TH/TH3.java
@@ -0,0 +1,32 @@
+package SS3_TH;
+
+import java.util.Scanner;
+
+public class TH3 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("nhập số a");
+        a = input.nextInt();
+        System.out.println("nhập số b");
+        b = input.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+                System.out.println("a =" + a);
+
+            } else {
+                b = b - a;
+                System.out.println("b =" + b);
+            }
+            System.out.println(a);
+        }
+    }
+}
